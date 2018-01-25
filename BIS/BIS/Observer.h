605b7201d824a2032f8caa6c7dc2db0a5c4d1476
Mabaria <rima15@student.bth.se>
2018-01-25 11:38:25 +0100

    Added test files

--- BIS/BIS/Observer.h
+++ BIS/BIS/Observer.h
@@ -1,14 +1,13 @@
 #pragma once
+#include <string>
 
 class Observer
 {
 public:
+	Observer() {}
+	virtual ~Observer() {}
 	virtual void Update(std::string eventString) = 0;
 
 
-
-
-
-private:
 
 };
