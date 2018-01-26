84411c2fa176de9e9752e74657d8fe71195c1472
Robin Westerlund <robin_westerlund@hotmail.com>
2018-01-26 14:12:54 +0100

    Fixade typecasting i Emils funktion. Testade att skapa panel. Rebase inc.

--- UI/UIElements/Test_Panel.cpp
+++ UI/UIElements/Test_Panel.cpp
@@ -1,7 +1,13 @@
 #include "Test_Panel.h"
 
-void Test_Panel()
+void Test_Panel3D()
 {
 	Window window(L"Test_Window", 1280, 720);
-	Panel3D panel(100, 100, 0, 0, window.GetWindow());	
+	Panel3D panel(640, 360, 0, 0, window.GetWindow());	
 }
+
+void Test_Panel2D()
+{
+	// TODO: Test 2D panel when finished.
+}
+
