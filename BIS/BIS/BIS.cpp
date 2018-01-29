76da1f00837fca541f0889aa726f68b9e8e6f8e4
Mabaria <rima15@student.bth.se>
2018-01-29 11:49:48 +0100

    Fixed missing BIS.cpp (?)

---
+++ BIS/BIS/BIS.cpp
@@ -0,0 +1,12 @@
+// BIS.cpp : Defines the entry point for the console application.
+//
+
+#include "../../GraphicsEngine/Test_Graphics.h"
+#include "../../IO/Test_Input.h"
+
+int main()
+{
+	Test_Input();
+
+	return 0;
+}
