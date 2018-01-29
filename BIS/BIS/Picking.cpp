523d25cd1c742f9c4cf9dd719e3256b5c5629e96
EMIL GRENEBRANT <emgc14@student.bth.se>
2018-01-29 11:12:45 +0100

    Added AABB functionality

--- BIS/BIS/Picking.cpp
+++ BIS/BIS/Picking.cpp
@@ -1,12 +1,32 @@
 #include "Picking.h"
+#include <algorithm>
+
+AABB FromIndexedMeshToAABB(
+	Mesh &mesh,	// Const
+	int index) 
+{
+	AABB retAABB;
+	bool x;
+	// Change this to reference (&)
+	std::vector<unsigned int> &indexVector =
+		mesh.GetIndexVectors()[index];
+
+	std::vector<Vertex> &vertexVector =
+		mesh.GetVertexVector();
+
+	for (unsigned int i = 0; i < 8; i++)
+	{
+		Vertex &currVert = vertexVector[indexVector[i]];
+
+	}
+}
 
 void Picking::GetWorldRay(
 	const DirectX::XMMATRIX &projectionMatrix,
 	const DirectX::XMMATRIX &viewMatrix,
 	float nScreenX,
 	float nScreenY,
-	DirectX::XMVECTOR &rRayOrigin,
-	DirectX::XMVECTOR &rRayDirection)
+	Ray &rRay)
 {
 	// Declaring variables used throughout
 	DirectX::XMMATRIX inverseProjection, inverseView;
@@ -32,10 +52,10 @@ void Picking::GetWorldRay(
 	DirectX::XMStoreFloat4x4(&fInverseView, inverseView);
 
 	// Extract camera position from view Matrix
-	rRayOrigin = inverseView.r[3];
+	rRay.origin = inverseView.r[3];
 
 	// Extract a direction from the view matrix
-	rRayDirection = {
+	rRay.direction = {
 		// x
 		(nScreenX * fInverseView._11) +
 		(nScreenY * fInverseView._21) +
@@ -52,4 +72,50 @@ void Picking::GetWorldRay(
 		0.0f	// Should maybe be 1.0f
 	};
 	return;
+}
+
+bool Picking::IsRayIntersectingAABB(
+	const Ray &ray,
+	const AABB &box)
+{
+	// Initializing variables
+	DirectX::XMFLOAT3 fRayOrigin, fRayDirection;
+	float tmin, tymin, tzmin;
+	float tmax, tymax, tzmax;
+
+	// Storing vectors as floats instead
+	DirectX::XMStoreFloat3(&fRayOrigin, ray.origin);
+	DirectX::XMStoreFloat3(&fRayDirection, ray.direction);
+
+	tmin = (box.x.min - fRayOrigin.x) / fRayDirection.x;
+	tmax = (box.x.max - fRayOrigin.x) / fRayDirection.x;
+
+	if (tmin > tmax)
+		std::swap(tmin, tmax);
+
+	tymin = (box.y.min - fRayOrigin.y) / fRayDirection.y;
+	tymax = (box.y.max - fRayOrigin.y) / fRayDirection.y;
+
+	if (tymin > tymax)
+		std::swap(tymin, tymax);
+
+	if ((tmin > tymax) || (tymin > tmax))
+		return false;
+
+	if (tymin > tmin)
+		tmin = tymin;
+
+	if (tymax < tmax)
+		tmax = tymax;
+
+	tzmin = (box.z.min - fRayOrigin.z) / fRayDirection.z;
+	tzmax = (box.z.max - fRayOrigin.z) / fRayDirection.z;
+
+	if (tzmin > tzmax)
+		std::swap(tzmin, tzmax);
+
+	if ((tmin > tzmax) || (tzmin > tmax))
+		return false;
+
+	return true;
 }
