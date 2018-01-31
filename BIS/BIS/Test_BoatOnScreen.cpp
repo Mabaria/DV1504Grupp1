78393710bda3833551f14c5968751b8e3f746ae8
EMIL GRENEBRANT <emgc14@student.bth.se>
2018-01-31 15:34:31 +0100

    WORK IN PROGRESS

---
+++ BIS/BIS/Test_BoatOnScreen.cpp
@@ -0,0 +1,8 @@
+#include "Test_BoatOnScreen.h"
+#include "../../GraphicsEngine/Parsing/Mesh.h"
+#include "../../UI/UIElements/"
+void TestBoatOnScreen()
+{
+	Mesh boatMesh("../../Models/TestBoat.obj");
+
+}
