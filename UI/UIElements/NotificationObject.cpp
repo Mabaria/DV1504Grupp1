97d57e99eec9a4dca69fbce73cca6a623506378b
Robin Westerlund <robin_westerlund@hotmail.com>
2018-02-09 13:36:45 +0100

    konstruktor och grejer

--- UI/UIElements/NotificationObject.cpp
+++ UI/UIElements/NotificationObject.cpp
@@ -1,6 +1,11 @@
 #include "NotificationObject.h"
 
-NotificationObject::NotificationObject(Room * room, LogEvent * event, Direct2D *direct2d) : mButton(direct2d, "", 0, 0, 0, 0), mTextBox(direct2d, 0, 0, 0, 0)
+NotificationObject::NotificationObject(
+	Room * room, 
+	LogEvent * event, 
+	Direct2D *direct2d) 
+	: mButton(direct2d, "", 0, 0, 0, 0)
+	, mTextBox(direct2d, 0, 0, 0, 0)
 {
 	this->mTimer = event->GetTimer();
 	this->mRoomName = room->GetName();
@@ -8,6 +13,27 @@ NotificationObject::NotificationObject(Room * room, LogEvent * event, Direct2D *
 	this->mStartTime = this->mTimer->WhenTimerStarted();
 	this->mElapsedTime = this->mTimer->GetTimeAsStr();
 	this->mEventType = event->GetType();
+	
+	// 200x80 notification object.
+	D2D1_RECT_F object_size;
+	object_size.top = 0;
+	object_size.left = 0;
+	object_size.right = 200;
+	object_size.bottom = 80;
+
+	// 40x40 pixel icon.
+	D2D1_RECT_F icon_size;
+	icon_size.top = 20;
+	icon_size.left = 10;
+	icon_size.right = 50;
+	icon_size.bottom = 60;
+
+	// 150x80 text box.
+	D2D1_RECT_F textbox_size;
+	textbox_size.top = 0;
+	textbox_size.left = 50;
+	textbox_size.right = 190;
+	textbox_size.bottom = 80;
 
 	std::string file_path = "../../Models/";
 	switch (this->mEventType)
@@ -27,7 +53,26 @@ NotificationObject::NotificationObject(Room * room, LogEvent * event, Direct2D *
 	default:
 		file_path += "pepehands.jpg";
 	}
-	this->mButton.CreateButton(file_path, 0, 0, 0, 0);
+
+	this->mButton.CreateButton(
+		file_path, 
+		object_size.left, 
+		object_size.top, 
+		object_size.right, 
+		object_size.bottom);
+
+	this->mButton.SetIconSize(
+		icon_size.left,
+		icon_size.top,
+		icon_size.right,
+		icon_size.bottom);
+
+	this->mTextBox.SetTextBoxSize(
+		textbox_size.left,
+		textbox_size.top,
+		textbox_size.right,
+		textbox_size.bottom);
+
 	this->mTextBox.SetText(this->GetNotificationString());
 }
 
@@ -72,6 +117,21 @@ const std::string NotificationObject::GetNotificationString() const
 	return_string += this->mElapsedTime;
 
 	return return_string;
+}
+
+const void NotificationObject::SetPosition(int x, int y)
+{
+	this->mButton.SetButtonsize(
+		x, 
+		y, 
+		this->mButton.GetButtonsize().right + x, 
+		this->mButton.GetButtonsize().bottom + y);
+	
+}
+
+const void NotificationObject::Move(int x, int y)
+{
+	return void();
 }
 
 void NotificationObject::Update()
--- UI/UIElements/NotificationObject.h
+++ UI/UIElements/NotificationObject.h
@@ -22,6 +22,10 @@ public:
 
 	const std::string GetNotificationString() const;
 
+	const void SetPosition(int x, int y);
+	const void Move(int x, int y);
+	const void SetText(std::string text);
+
 	void Update();
 
 private:
