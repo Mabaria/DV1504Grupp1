19130766b66959bee203286ff0d94babb27c900d
Robin Westerlund <robin_westerlund@hotmail.com>
2018-02-09 09:32:12 +0100

    fu

--- UI/UIElements/NotificationObject.cpp
+++ UI/UIElements/NotificationObject.cpp
@@ -7,35 +7,3 @@ NotificationObject::NotificationObject()
 NotificationObject::~NotificationObject()
 {
 }
-
-void NotificationObject::AddButton(int width, int height, int top, int left, std::string imageFilePath, std::string buttonName)
-{
-	{
-		this->mButtonNames.push_back(buttonName); // Add to names list
-		Button newButton(this->mDirect2D,
-			imageFilePath,
-			left,
-			top,
-			left + width,
-			top + height);
-		this->mButtonVector.push_back(newButton); // Add button
-	}
-}
-
-void NotificationObject::AddTextbox(int width, int height, int top, int left, std::string text, std::string name)
-{
-	{
-		this->mTextBoxNames.push_back(name); // Add name.
-
-		TextBox *newTextBox = new TextBox(
-			this->mDirect2D,
-			left,
-			top,
-			left + width,
-			top + height);
-		this->mTextBoxVector.push_back(newTextBox); // Add text box.
-
-		this->mTextBoxVector.back()->SetText(text); // Set text.
-	}
-
-}
--- UI/UIElements/NotificationObject.h
+++ UI/UIElements/NotificationObject.h
@@ -9,23 +9,7 @@ public:
 	~NotificationObject();
 
 private:
-	std::vector<Button*> mButtonVector;
-	std::vector<std::string> mButtonNames;
 
 
-	void AddButton(int width,
-		int height,
-		int top,
-		int left,
-		std::string imageFilePath,
-		std::string buttonName);
-
-	void AddTextbox(
-		int width,
-		int height,
-		int top,
-		int left,
-		std::string text,
-		std::string name);
 
 };
