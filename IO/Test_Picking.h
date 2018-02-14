6bc63fe6ce6b5b62a8c02c6a09efd6d0f9261f71
EMIL GRENEBRANT <emgc14@student.bth.se>
2018-02-14 10:16:00 +0100

    Removed some depricated functions from Test_Picking

--- IO/Test_Picking.h
+++ IO/Test_Picking.h
@@ -5,21 +5,6 @@
 #include "../GraphicsEngine/Window.h"
 #include "../UI/UIElements/Panel3D.h"
 #include "../IO/Mouse.h"
-#include <cmath>
-#include <math.h>
 #include <iostream>
-
-void Test_Picking();
-void Test_Picking_ClickToRay_Perspective();
-void Test_Picking_ClickToRay_Orthographic();
-
-float DotProduct(DirectX::XMVECTOR vec1, DirectX::XMVECTOR vec2);
-
-// Help functions for Click To Ray testing
-bool CheckRay_Orthographic(
-	Ray ray,
-	DirectX::XMVECTOR pos,
-	DirectX::XMVECTOR dir);
-DirectX::XMVECTOR ComputePosition(Camera *pCamera, float posX, float posY);
 
 bool TestPickingOnBoat();
