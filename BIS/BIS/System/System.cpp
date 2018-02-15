54060ec95de6d02d757f285d76ad22543b73b254
Filip <craiynel@gmail.com>
2018-02-15 10:09:26 +0100

    Changed text3d pictures

--- BIS/BIS/System/System.cpp
+++ BIS/BIS/System/System.cpp
@@ -211,7 +211,6 @@ void System::mSetupPanels()
 		120.f, 32.f/9.f,
 		0.1f, 100.0f, LOOK_AT, PERSPECTIVE);
 	this->mpSideViewPanel->SetCamera(this->mpSideViewCamera);
-	this->mpTopViewPanel->SetCamera(this->mpTopViewCamera);
 
 	// Creating and setting the shaders.
 	this->mpSideViewPanel->CreateShadersAndSetup(
@@ -286,21 +285,21 @@ void System::mSetupModels()
 		"Text3D_Floor01",
 		this->mTexts[0]->GetIndices(),
 		this->mTexts[0]->GetVertices(),
-		L"../../Models/däck01.dds"
+		L"../../Models/d01.dds"
 	); 
 	
 	this->mpTopViewPanel->AddMeshObject(
 		"Text3D_Floor1", 
 		this->mTexts[0]->GetIndices(), 
 		this->mTexts[0]->GetVertices(), 
-		L"../../Models/Däck1.dds"
+		L"../../Models/d1.dds"
 	);
 	
 	this->mpTopViewPanel->AddMeshObject(
 		"Text3D_Floor2",
 		this->mTexts[0]->GetIndices(),
 		this->mTexts[0]->GetVertices(),
-		L"../../Models/däck2.dds"
+		L"../../Models/d2.dds"
 	);
 
 
@@ -317,33 +316,33 @@ void System::mSetupModels()
 
 
 	this->mpTopViewPanel->rGetMeshObject("Text3D_Floor01")->
-		Scale(scale * 2.8f, scale * 1.2f, scale * 3.5f);
+		Scale(scale * 2.0f, scale * 1.2f, scale * 3.5f);
 
 	this->mpTopViewPanel->rGetMeshObject("Text3D_Floor01")->
-		Rotate(XM_PI / 2, XM_PI / 2, 0.0f);
+		Rotate(XM_PI / 2.0f, 0.0f, 0.0f);
 
 	this->mpTopViewPanel->rGetMeshObject("Text3D_Floor01")->
-		Translate(0.7f, 0.0f, -0.35f);
+		Translate(0.6f, 0.0f, -0.5f);
 
 
 	this->mpTopViewPanel->rGetMeshObject("Text3D_Floor1")->
-		Scale(scale * 2.8f, scale * 1.2f, scale * 3.5f);
+		Scale(scale * 1.0f, scale * 1.2f, scale * 3.5f);
 
 	this->mpTopViewPanel->rGetMeshObject("Text3D_Floor1")->
-		Rotate(XM_PI / 2, XM_PI / 2, 0.0f);
+		Rotate(XM_PI / 2.0, 0.0f, 0.0f);
 
 	this->mpTopViewPanel->rGetMeshObject("Text3D_Floor1")->
-		Translate(0.7f, 0.0f, 0.15f);
+		Translate(0.75f, 0.0f, -0.05f);
 
 
 	this->mpTopViewPanel->rGetMeshObject("Text3D_Floor2")->
-		Scale(scale * 2.8f, scale * 1.2f, scale * 3.5f);
+		Scale(scale * 1.0f, scale * 1.2f, scale * 3.5f);
 
 	this->mpTopViewPanel->rGetMeshObject("Text3D_Floor2")->
-		Rotate(XM_PI / 2, XM_PI / 2, 0.0f);
+		Rotate(XM_PI / 2.0f, 0.0f, 0.0f);
 
 	this->mpTopViewPanel->rGetMeshObject("Text3D_Floor2")->
-		Translate(0.7f, 0.0f, 0.65f);
+		Translate(0.75f, 0.0f, 0.45f);
 
 	
 
